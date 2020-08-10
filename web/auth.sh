a81eecd8919db4c3dbc6b6d623751f94663faa47
Quinn <qcasey1@gmail.com>
2020-08-10 03:21:13 +0000

    remove unused

--- web/auth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-curl -H "Content-Type:application/json" -X POST -d '{
-	"isPersistent":true,
-	"password":"password",
-	"username":"username"
-}' https://monitor.us.sunpower.com/CustomerPortal/Auth/Auth.svc/Authenticate
--- web/getModuleInfo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --silent "https://monitor.us.sunpower.com/CustomerPortal/SystemInfo/SystemInfo.svc/getACPVModuleInfo?id=7cc286af-0ce7-49cd-8e2a-b4c9a24d68a3"
